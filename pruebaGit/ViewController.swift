a6e55599a954e51c09c9c5756d9ed0faef0f0561
Carlos Eduardo Muñoz González <Charlie@ing-eduardo-munoz.local>
2016-05-31 19:13:40 -0500

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Carlos Eduardo Muñoz González on 5/31/16.
+//  Copyright © 2016 Eduardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
